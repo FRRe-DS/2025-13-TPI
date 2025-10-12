0ef3f2f3a66c2cfe5a8f4f1a95ed3e8f059e72ad
BenjaNeu <benjaneu2004@gmail.com>
2025-10-12 17:29:58 -0300

    .

--- compras/backend/app/models/user.py
+++ compras/backend/app/models/user.py
@@ -25,3 +25,4 @@ class User(Base):
 
     # relación uno-a-uno / uno-a-muchos desde el lado "padre"
     profiles = relationship("userProfile", back_populates="user", uselist=False, cascade="all, delete-orphan")
+    orders = relationship("Order", back_populates="user", cascade="all, delete-orphan")
