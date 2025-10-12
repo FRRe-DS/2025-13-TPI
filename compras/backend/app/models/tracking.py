43bdd7417fcf7cb2f68186b4503f9d004b35daae
BenjaNeu <benjaneu2004@gmail.com>
2025-10-12 17:22:42 -0300

    .

---
+++ compras/backend/app/models/tracking.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
+from ..db import Base
+
+class tracking(Base):
+    __tablename__ = 'tracking'
+    id = Column(Integer, primary_key=True, index=True)
+    order_id = Column(Integer, ForeignKey("order.id", ondelete="CASCADE"), nullable=False)
+    status = Column(String)
+    location = Column(String)
+    creationDate = Column(DateTime)
+
+    # relación desde el lado "hijo"
+    order = relationship("Order", back_populates="tracking")
--- compras/backend/app/models/user_profiles.py
+++ compras/backend/app/models/user_profiles.py
@@ -1,9 +1,11 @@
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship
 from ..db import Base
 
 class User_Profile(Base):
-    __tablename__ = 'user_profiles'
+    __tablename__ = 'userProfiles'
     id = Column(Integer, primary_key=True, index=True)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
     email = Column(String, unique=True, index=True)
     phone = Column(Integer, unique=True)
     dni = Column(Integer, unique=True)
@@ -11,5 +13,8 @@ class User_Profile(Base):
     address = Column(String)
     created_at = Column(DateTime)
     updated_at = Column(DateTime)
+
+    # relación desde el lado "hijo"
+    user = relationship("User", back_populates="userProfile")
 
     
