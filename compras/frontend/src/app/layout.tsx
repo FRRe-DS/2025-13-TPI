bf73953cf2bbaa7366fd90884724cb4b3aa0d05d
gabriel <cuencag95@gmail.com>
2025-11-28 15:59:52 -0300

    .

--- compras/frontend/src/app/layout.tsx
+++ compras/frontend/src/app/layout.tsx
@@ -3,20 +3,20 @@
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
 import Navbar from "@/components/Navbar";
-import { usePathname } from "next/navigation";   // 👈 esta import
+import { usePathname } from "next/navigation";   
 import type { ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const pathname = usePathname();
 
-  // 👇 en qué rutas NO quiero mostrar la navbar
-  const hideNavbar = pathname === "/login";      // sin barra final
+ 
+  const hideNavbar = pathname === "/login";      
 
   return (
     <html lang="es">
       <body>
         <AuthProvider>
-          {/* Solo muestro la navbar si NO estoy en /login */}
+         
           {!hideNavbar && <Navbar />}
 
           <main>{children}</main>
